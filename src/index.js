1ee77338bec82de024365e1b2982b57e3a232cca
Oksana Tkachenko <oksana@crystax.net>
2019-03-20 20:59:26 +0200

    added style for adding tourist

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import 'bootstrap';
 import $ from 'jquery';
+
+$(document).ready(function() {
+
+  //quantity increment
+  $(".guest-detail").click(function() {
+    $(this).toggleClass('active');
+    $("#guest-form").toggleClass("d-block");
+  });
+});
